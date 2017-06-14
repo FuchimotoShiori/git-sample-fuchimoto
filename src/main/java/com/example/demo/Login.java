1d63804a53a7e412fe36c7b704bc7011f4053222
shiori.fuchimoto <shiori.fuchimoto@rakus.co.jp>
2017-06-14 14:02:54 +0900

    Login.javaの新規作成。メインメソッド内はコメントのみ。

---
+++ src/main/java/com/example/demo/Login.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+public class Login {
+
+	private int id;
+	private String name;
+	private String mailAddress;
+	
+	public static void main(){
+		
+		//ログイン処理したい。
+		
+	}
+}
