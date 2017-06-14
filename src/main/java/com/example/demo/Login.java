2066021df057d601600b7d3f72a6792d975cf4d6
takuya.hizukuri <takuya.hizukuri@rakus.co.jp>
2017-06-14 14:02:54 +0900

    ログイン機能の実装.

---
+++ src/main/java/com/example/demo/Login.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+public class Login {
+	private static String name;
+	private static String id;
+	private static String passworld;
+
+	public static void main(String[] args) {
+		name = "hage";
+		id = "hoge";
+		passworld = "page";
+		System.out.println(name + id + passworld);
+	}
+}
